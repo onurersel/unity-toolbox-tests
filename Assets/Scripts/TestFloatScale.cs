34bd8234c405d92c1a2aede7396fae07f2af5f57
Onur Ersel <onur.ersel@tribalistanbul.com>
2015-03-18 19:58:06 +0200

    test

---
+++ Assets/Scripts/TestFloatScale.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFloatScale : MonoBehaviour {
+
+	public TBFloatScale floatScale;
+	public TBFloatScale floatScal2;
+
+
+	void Update()
+	{
+		floatScale.Scale(Input.mousePosition.x);
+	}
+}
